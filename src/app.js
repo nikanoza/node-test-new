02c1b6fd1d262fa76ae769dccb130af72ad93578
nixena <nikanozadze1010@gmail.com>
2024-09-08 20:41:09 +0400

    feat: add database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
 import { users } from "./data.js";
+import pool from "./config/database.js";
 
+dotenv.config();
 const app = express();
+
+const init = async () => {
+  try {
+    const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS USERS (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        email VARCHAR(100) UNIQUE,
+        age INT
+      );
+    `;
+
+    await pool.query(createTableQuery);
+  } catch (error) {
+    console.error("Error creating USERS table:", error);
+  }
+};
+
+init();
 
 app.get("/users", async (_, res) => {
   return res.status(200).json({ message: "Api requests!", users });
